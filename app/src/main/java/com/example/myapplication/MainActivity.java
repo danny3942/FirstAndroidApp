1262fdbf752a574dfb21170feed90656d6872e03
BuildTools <unconfigured@null.spigotmc.org>
2020-11-29 11:59:15 -0600

    Got the few bugs worked out and added some images to mess with visibility attribute

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,65 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public Context context;
     private int counter = 0;
-    private TextView message = findViewById(R.id.messageText);
-    private Button counterButton = findViewById(R.id.buttonId);
+    public Button counterButton;
+    public TextView message;
+    public Button redButton;
+    public Button yellowButton;
+    public Button greenButton;
+    public ImageView redImage;
+    public ImageView yellowImage;
+    public ImageView greenImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        message = findViewById(R.id.messageText);
+        counterButton = findViewById(R.id.buttonId);
+        redButton = findViewById(R.id.buttonRed);
+        yellowButton = findViewById(R.id.buttonYellow);
+        greenButton = findViewById(R.id.buttonGreen);
+        redImage = findViewById(R.id.imageRed);
+        yellowImage = findViewById(R.id.imageYellow);
+        greenImage = findViewById(R.id.imageGreen);
         counterButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 counter++;
                 message.setText("You pressed the button " + counter + "Times!");
+            }
+        });
+        redButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (redImage.getVisibility() == View.INVISIBLE)
+                    redImage.setVisibility(View.VISIBLE);
+                else
+                    redImage.setVisibility(View.INVISIBLE);
+            }
+        });
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (yellowImage.getVisibility() == View.INVISIBLE)
+                    yellowImage.setVisibility(View.VISIBLE);
+                else
+                    yellowImage.setVisibility(View.INVISIBLE);
+            }
+        });
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (greenImage.getVisibility() == View.INVISIBLE)
+                    greenImage.setVisibility(View.VISIBLE);
+                else
+                    greenImage.setVisibility(View.INVISIBLE);
             }
         });
     }
